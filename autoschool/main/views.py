f9cc0918f920ccc5fb447bedc23720aa834695ef
Mitsuhano <mitsuhanocreations@gmail.com>
2025-10-28 01:08:10 +0200

    feat: update

--- autoschool/main/views.py
+++ autoschool/main/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'main/components/home.html')
+    active_page = request.GET.get('active_page', 'home')
+    return render(request, 'main/components/home.html', {'active_page': active_page})
 
 def about(request):
-    return render(request, 'main/components/about.html')
+    active_page = request.GET.get('active_page', 'about')
+    return render(request, 'main/components/about.html', {'active_page': active_page})
 
 def booking(request):
-    return render(request, 'main/components/booking.html')
+    active_page = request.GET.get('active_page', 'booking')
+    return render(request, 'main/components/booking.html', {'active_page': active_page})
 
 def contact(request):
-    return render(request, 'main/components/contact.html')
+    active_page = request.GET.get('active_page', 'contact')
+    return render(request, 'main/components/contact.html', {'active_page': active_page})
