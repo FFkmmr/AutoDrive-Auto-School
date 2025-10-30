9f7fe5a6d6b5035e17031d897b2213a3c48d9597
Mitsuhano <mitsuhanocreations@gmail.com>
2025-10-30 21:14:49 +0200

    Fix Procfile

--- autoschool/autoschool/wsgi.py
+++ autoschool/autoschool/wsgi.py
@@ -1,16 +1,14 @@
-"""
-WSGI config for autoschool project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+
+# Добавляем в sys.path папку, где лежат приложения (чтобы импорты вроде "main" работали)
+# __file__ указывает на /app/autoschool/autoschool/wsgi.py, поэтому добавляем ../ (то есть /app/autoschool)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if BASE_DIR not in sys.path:
+    sys.path.insert(0, BASE_DIR)
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'autoschool.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'autoschool.autoschool.settings')
 
 application = get_wsgi_application()
